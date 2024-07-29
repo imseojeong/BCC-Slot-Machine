871f231627414dc8db17d6182916635e94bb8041
danbi01 <twobi01@gmail.com>
2024-07-29 19:18:43 +0900

    Add ScoreButton

--- Assets/Scripts/ScoreButton.cs
+++ Assets/Scripts/ScoreButton.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ScoreButton : MonoBehaviour
 {
     public void ScoreButtonClickHandler()
     {
-        SceneManager.LoadScene("Score Scene");
+        SceneManager.LoadScene("Start Scene");
         Debug.Log("Click ScoreButton");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter ScoreButton");
+        this.transform.Rotate(0, 0, 3);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit StartButton");
+        this.transform.Rotate(0, 0, -3);
     }
 
     void Start()
