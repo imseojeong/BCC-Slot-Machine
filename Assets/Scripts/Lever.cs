fea42b1235c1db8a9f7288b81a04a254dcccf454
danbi01 <twobi01@gmail.com>
2024-11-20 21:48:05 +0900

    레버 연타 시 오류 방지

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -37,7 +37,7 @@ public class Lever : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         // 레버 버튼 비활성화
         GetComponent<Button>().interactable = false;
         // 1초 기다림
-        yield return new WaitForSeconds(NeedleMove.machineStoppedDuration);
+        yield return new WaitForSeconds(NeedleMove.machineStoppedDuration + 0.1f);
         // 레버 버튼 활성화
         GetComponent<Button>().interactable = true;
     }
